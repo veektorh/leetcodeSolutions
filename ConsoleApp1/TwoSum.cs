172bf8fd215dbfbcf399e57ce1e68c7d2f2cca8f
Victor Bolum <victor.bolum@cyberspace.net.ng>
2020-02-27 14:20:42 +0100

    Two Sum:

---
+++ ConsoleApp1/TwoSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class TwoSum
+    {
+        public static int[] Solution(int[] nums, int target)
+        {
+            var temp_list = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var currentValue = nums[i];
+
+                var otherHalf = target - currentValue;
+
+                if (temp_list.ContainsKey(otherHalf))
+                {
+                    return new int[] { temp_list[otherHalf], i };
+                }
+
+                if (!temp_list.ContainsKey(currentValue))
+                {
+                    temp_list.Add(currentValue, i);
+                }
+
+
+            }
+
+            return new int[] { };
+        }
+    }
+}
